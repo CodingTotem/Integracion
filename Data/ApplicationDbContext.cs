a67039e491b14e72a6f97ad6fd28d39019287268
Carlos Piedra <barce90@hotmail.com>
2024-06-11 22:20:33 -0700

    Agregar Precio Promocional del Producto - Actualizado

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,15 +4,19 @@ using Models.Entities;
 namespace Data
 {
 
-    public class ApplicationDbContext : DbContext
-    {
-
-        public ApplicationDbContext(DbContextOptions options) : base(options)
+        public class ApplicationDbContext : DbContext
         {
+
+                public ApplicationDbContext(DbContextOptions options) : base(options)
+                {
+                }
+
+
+                public DbSet<Categoria> Categorias { get; set; }
+
+                public DbSet<Marca> Marca { get; set; }
+
+                public DbSet<Producto> Productos { get; set; }
         }
-
-
-        public DbSet<Categoria> Categorias { get; set; }
-    }
 
 }
